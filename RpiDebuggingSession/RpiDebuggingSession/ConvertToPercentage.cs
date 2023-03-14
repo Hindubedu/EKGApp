fbdc9385b1d796e03dce912c9ba46525c04311f6
Hindubedu <40704028+Hindubedu@users.noreply.github.com>
2023-03-14 10:50:22 +0100

    Add files via upload
    Rpi code added

---
+++ RpiDebuggingSession/RpiDebuggingSession/ConvertToPercentage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpiDebuggingSession
+{
+    public class ConvertToPercentage
+    {
+        public int ConvertTo100(short value)
+        {
+            var result = Convert.ToDouble(value) / 4096.0 * 100.0;
+
+            return (int)result;
+        }
+    }
+}

aa0c5ceecdcdea17994fb020e3ea7ca5cd9a4982
Hindubedu <40704028+Hindubedu@users.noreply.github.com>
2023-03-17 12:36:18 +0100

    Tester

--- EKGApp/MainWindow.xaml.cs
+++ EKGApp/MainWindow.xaml.cs
@@ -56,7 +56,7 @@ namespace EKGApp
             DataContext = this;
         }
 
-        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        private void LoadButton_Click(object sender, RoutedEventArgs e) //Change
         {
             EKGLine.Values.Clear();
             RRList.Clear();
@@ -82,7 +82,7 @@ namespace EKGApp
 
                         if (splitLine.Length == 2)
                         {
-                            EKGLine.Values.Add(Double.Parse(doubleValues));
+                            EKGLine.Values.Add(Double.Parse(doubleValues)); 
                             RRList.Add(Double.Parse(doubleValues));
                         }
                         else

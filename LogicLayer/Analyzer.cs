9e63a1e9496a3d3555344daf41a110c9240770cc
ingridhornshoj <115166856+ingridhornshoj@users.noreply.github.com>
2023-04-23 23:19:11 +0200

    sPeak detektion

--- LogicLayer/Analyzer.cs
+++ LogicLayer/Analyzer.cs
@@ -24,10 +24,22 @@ namespace LogicLayer
                 Histogram histogram = new Histogram();
                 double baseline = histogram.FindBaseLine(RRList);
 
+                // Lav listen med målinger om til liste af koordinater (x = index/nr. måling, y=måling)
+                List<Coordinates> signal = new();
+                for (int index = 0; index < RRList.Count; index++)
+                {
+                    signal.Add(new Coordinates(index, RRList[index]));
+                }
+
+                // Udregn half_peak_width til 0,5% af målinger. Dette er halvdelen af et peaks bredde.
+                // Dette bruges til at fjerne målinger omkring et peak, når det er fundet, så man ikke finder samme peak flere gange.
+                // Jo mindre % her, desto mere sensitiv er detektoren for peaks.
+                int half_peak_width = (int)(signal.Count * 0.005);
+
                 //Find R- og S-takker
                 PeakDetector peakDetector = new PeakDetector();
-                List<Coordinates> rPeaks = peakDetector.DetectRPeaks(RRList, baseline);
-                //List<double> sPeaks = peakDetector.DetectSPeaks(RRList, rPeaks);
+                List<Coordinates> rPeaks = peakDetector.DetectRPeaks(signal, baseline, half_peak_width);
+                List<Coordinates> sPeaks = peakDetector.DetectSPeaks(signal, rPeaks, half_peak_width);
 
                 return true;
             }
@@ -46,23 +58,12 @@ namespace LogicLayer
         
         internal class PeakDetector
         {
-            public List<Coordinates> DetectRPeaks(List<double> signal, double baseline)
+            public List<Coordinates> DetectRPeaks(List<Coordinates> signal, double baseline, int half_peak_width)
             {
-
-                // Lav listen med målinger om til liste af koordinater (x = index/nr. måling, y=måling)
-                List<Coordinates> rPeaks = new();
-                for(int index = 0; index < signal.Count; index++)
-                {
-                    rPeaks.Add(new Coordinates(index, signal[index]));
-                }
-
                 // Fjern alle målinger, hvor målingen (y-aksen) er under tærskel (0,5 mV over baseline)
                 double threshold = 0.5 + baseline;
-                rPeaks.RemoveAll(m => m.Y < threshold);
+                List<Coordinates> rPeaks = signal.Where(m => m.Y >= threshold).ToList();
                 
-                // Udregn half_peak_width til 0,5% af målinger. Dette er halvdelen af et peaks bredde.
-                // Dette bruges til at fjerne målinger omkring et peak, når det er fundet, så man ikke finder samme peak flere gange.
-                int half_peak_width = (int)(signal.Count * 0.005);
 
                 // Lav en tom liste, hvor vi gemmer funde peaks
                 List<Coordinates> foundRPeaks = new();
@@ -81,60 +82,23 @@ namespace LogicLayer
                 return foundRPeaks;
             }
 
-            public List<double> DetectSPeaks(List<double> signal, List<(double, double)> rPeaks)
+            public List<Coordinates> DetectSPeaks(List<Coordinates> signal, List<Coordinates> rPeaks, int half_peak_width)
             {
-                List<double> foundSPeaks = new List<double>();
+                List<Coordinates> foundSPeaks = new();
 
                 // Algoritme til s-takker 
                 // For hver R-peak: find den laveste y-værdi mellem r-peak og r-peak + half_peak_width    
-                
-                //foreach (double rPeak in rPeaks)
-                //{
-                //    //signal.GetRange(rPeaks)
-                //}
 
+                foreach (Coordinates rPeak in rPeaks)
+                {
+                    Coordinates sPeak = signal.Where(m => m.X > rPeak.X && m.X < rPeak.X + half_peak_width).MinBy(c => c.Y);
+                    foundSPeaks.Add(sPeak);
+                }
 
                 return foundSPeaks;
             }
         }
 
-        //internal class PulseDetector
-        //{
-        //    public static double CalculatePulse(List<double> RRList)
-        //    {
-        //        bool belowThreshold = true;
-        //        int Rtak_old = 0;
-        //        int Rtak_new = 0;
-        //        double threshold = 0.5; //Antager at threshold er 0.5
-        //        double sample = 0.002; //Antager at sample rate er 0.002
-
-        //        List<double> RPeaks = new List<double>();
-
-        //        for (int i = 0; i < RRList.Count; i++)
-        //        {
-        //            double RR = RRList[i];
-
-        //            if (RR < threshold && belowThreshold)
-        //            {
-        //                belowThreshold = false;
-        //                Rtak_new = i;
-
-        //                if (Rtak_old != 0)
-        //                {
-        //                    RPeaks.Add((Rtak_new - Rtak_old) * sample * 60);
-        //                }
-        //                Rtak_old = Rtak_new;
-        //            }
-        //            else if (RR >= threshold && !belowThreshold)
-        //            {
-        //                belowThreshold = true;
-        //            }
-        //        }
-
-        //        double pulse = RPeaks.Count > 0 ? RPeaks.Average() : 0;
-        //        return pulse;
-        //    }
-        //}
     }
 }
 
